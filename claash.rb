879c86f9fcb583c6b08e9dfeb5d88e7c108de567
Peter Winton <pwinton@indigobio.com>
2014-02-11 07:22:51 -0500

    latency

--- claash.rb
+++ claash.rb
@@ -29,19 +29,50 @@ class Client < EM::Connection
     @pipe.pipe_in(bytes)
   end
 
+  def request(req, &block)
+    send_data(MsgPipe.write(req))
+    @on_msg = block
+  end
+
   def receive_msg(msg)
-    @count += 1
-    if @count == MSG_COUNT
-      Client.inc_completed_client_count
-      if Client.completed_client_count == CLIENT_COUNT
-        elapsed = Time.now - START
-        puts "elapsed seconds: #{elapsed}"
-        puts "milliseconds/msg: #{elapsed / (CLIENT_COUNT * MSG_COUNT) * 1000}"
-        EM.stop
+    if @on_msg
+      handler = @on_msg
+      @on_msg = nil # must clear before handler in case handler calls request()
+      handler.(msg)
+    else
+      @count += 1
+      if @count == MSG_COUNT
+        Client.inc_completed_client_count
+        if Client.completed_client_count == CLIENT_COUNT
+          elapsed = Time.now - START
+          puts "elapsed seconds: #{elapsed}"
+          puts "milliseconds/msg: #{elapsed / (CLIENT_COUNT * MSG_COUNT) * 1000}"
+          EM.stop
+        end
       end
     end
   end
+end
 
+def average(xs)
+  xs.inject{ |sum, el| sum + el }.to_f / xs.size
+end
+
+def roundtrip(client, remaining, latencies = [])
+  request = Ib::Mutex::V1::Request.new(requestor: 'req', owner_name: 'own', resource: Time.now.iso8601(6))
+  client.request(request) do |response|
+    if remaining > 0
+      roundtrip(client, remaining - 1, latencies.push(Time.now - Time.parse(response.request.resource)))
+    else
+      puts "average latency (ms): #{average(latencies) * 1000}"
+      EM.stop
+    end
+  end
+end
+
+EM.run do
+  client = EM.connect('127.0.0.1', 7890, Client)
+  roundtrip(client, MSG_COUNT)
 end
 
 EM.run do
