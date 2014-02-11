6bb85c821b8e0a1a2f020e38161c65e657c24f22
Peter Winton <pwinton@indigobio.com>
2014-02-10 20:52:55 -0500

    start of staash

--- lib/messaging.rb
+++ lib/messaging.rb
@@ -4,3 +4,13 @@ require 'eventmachine'
 require 'mercury'
 require 'messages'
 require 'eventmachine_util'
+
+# hack to fix equality failure in awesome_print
+# beefcake's default == assumes the RHS is another beefcake message
+module Beefcake
+  module Message
+    def ==(o)
+      super
+    end
+  end
+end
--- protodump.rb
+++ protodump.rb
@@ -4,16 +4,6 @@ require 'base64'
 require 'ap'
 require 'pp'
 
-# hack to fix equality failure in awesome_print
-# beefcake's default == assumes the RHS is another beefcake message
-module Beefcake
-  module Message
-    def ==(o)
-      super
-    end
-  end
-end
-
 if ARGV[0]
   msg = WireSerializer.read(Base64.decode64(ARGV[0]))
   ap WireSerializer.to_hash(msg), sort_keys: true
